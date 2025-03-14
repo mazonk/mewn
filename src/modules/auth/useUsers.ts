f84610e423f129d801197326aab1b21474accccd
Katarína Brezániová <katkabrezani@gmail.com>
2025-03-14 19:53:41 +0100

    video 14

--- src/modules/auth/useUsers.ts
+++ src/modules/auth/useUsers.ts
@@ -10,4 +10,46 @@ export const useUsers = () => {
   const email = ref<string | null>(null);
   const password = ref<string | null>(null);
   const registerDate = ref<string | null>(null);
+
+  const fetchToken = async (email: string, password: string): Promise<void> => {
+    try {
+      error.value = null;
+      const response = await fetch('https://ments-restapi.onrender.com/api/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'auth-token': localStorage.getItem('lsToken') || '',
+        },
+        body: JSON.stringify({ email, password }),
+        });
+      if (!response.ok) {
+        const errorResponse = await response.json();
+        console.log(errorResponse.error || 'Error');
+        throw new Error('Failed to fetch token');
+      }
+      const authResponse = await response.json();
+      token.value = authResponse.token;
+      users.value = authResponse.user;
+      isLoggedIn.value = true
+
+      localStorage.setItem('lsToken', authResponse.data.token);
+      localStorage.setItem('userIDToken', JSON.stringify(authResponse.data.user));
+      console.log("Token fetched:", token.value);
+    } catch (err) {
+      error.value = (err as Error).message;
+      isLoggedIn.value = false
+    }
+  };
+
+  return {
+    token,
+    isLoggedIn,
+    error,
+    users,
+    name,
+    email,
+    password,
+    registerDate,
+    fetchToken
+  };
 }
